21dab389b54d537132a6c07ec4b17b1fc9b2f9cf
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-06-18 23:03:26 -0300

    Part 4

---
+++ src/markdownPages/myFirstPost.md
@@ -0,0 +1,7 @@
+---
+title: "My First Post"
+date: "2017-08-21"
+description: "My first Post :)"
+---
+
+Aprendendo mais sobre gatsby.
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,25 +1,27 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const UsingDSG = () => (
-  <Layout>
-    <Seo title="Using DSG" />
-    <h1>
-      Hello from a <b>DSG Page</b>
-    </h1>
-    <p>This page is not created until requested by a user.</p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
-        documentation about Deferred Static Generation
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
 
-export default UsingDSG
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
